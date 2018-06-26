ce82732e3867545f8ea01cdf2dd5c41540ed404c
Keylonnie Miller <keylonniem@fb.com>
2018-06-26 13:11:16 -0700

    Initial Commit

---
+++ Tippy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tippy
+//
+//  Created by Keylonnie Miller on 6/26/18.
+//  Copyright © 2018 Keylonnie Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
